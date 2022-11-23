39707a457cf705d219a6d11cbc9175ac0e337122
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-11-23 15:19:24 +0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import React from 'react';
 import { Box, Slider } from '@mui/material';
 import Icon, { Stack } from '@mdi/react';
-import { mdiHeadphones } from '@mdi/js';
+import { } from '@mdi/js';
 import './App.css';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 
 function valuetext(value) {
@@ -11,36 +11,44 @@ function valuetext(value) {
 }
 
 function App() {
-	const [show, setShow] = useState(false);
-
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
-
 	const [value, setValue] = React.useState([40, 67]);
+
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
 
 	return (
 		<>
-			<Button variant="primary" onClick={handleShow}>
-				Launch demo modal
-			</Button>
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link href="#features">Features</Nav.Link>
+							<Nav.Link href="#pricing">Pricing</Nav.Link>
+							<NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">
+									Another action
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">
+									Separated link
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Nav>
+							<Nav.Link href="#deets">More deets</Nav.Link>
+							<Nav.Link eventKey={2} href="#memes">
+								Dank memes
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>Modal heading</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant="primary" onClick={handleClose}>
-						Save Changes
-					</Button>
-				</Modal.Footer>
-			</Modal>
 
 			<Box sx={{ width: 300 }}>
 				<Slider
@@ -51,8 +59,6 @@ function App() {
 					getAriaValueText={valuetext}
 				/>
 			</Box>
-
-			<Icon path={mdiHeadphones} style={{ width: '50px' }} />
 		</>
 	);
 }
